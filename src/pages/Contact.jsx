3e38e3578d4bd520a09303d92c1cdcd7e13b5dbe
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-17 15:36:10 +0200

    add white background under pop cards

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,10 +22,12 @@ export default function Contact() {
         }
     }, [location]);
 
+    
+
     return (
         <div className="outerBox">
             <div className="innerBox">
-                <div className="contactPopisek">
+                <div className={`contactPopisek outerBox`}>
                     <h2>Reach Out<br/> and <span>Start Today</span></h2>
                     <p>Whether you're ready to begin your <span>free trial</span>, explore our plans, or have questions about how <span>DataVizor can support your business</span>, we're here to help. Select the plan that fits your needs and reach out to our team for more information. We're excited to <span>connect</span> and help you harness the <span>full power of your data</span>.</p>
                 </div>
@@ -33,7 +35,7 @@ export default function Contact() {
                     <FormularComponent />
                 </div>
             </div>
-            <div className="contactInfo">
+            <div className={`contactInfo`}>
                 <a href="https://www.google.com/maps/dir//North+Hills+Office+Park,+3630+N+Hills+Dr,+Austin,+TX+78731,+Spojen%C3%A9+st%C3%A1ty/@30.3533916,-97.7568409,17.23z/data=!4m17!1m7!3m6!1s0x8644cb1a36c8bfc3:0xd42fccd86ca6f930!2sNorth+Hills+Office+Park!8m2!3d30.3532238!4d-97.7566986!16s%2Fg%2F11fp9mgx9l!4m8!1m0!1m5!1m1!1s0x8644cb1a36c8bfc3:0xd42fccd86ca6f930!2m2!1d-97.7567132!2d30.3531153!3e3?hl=cs&entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D" target="_isBlank"><img src="images/map3.png"/></a>
                 <div className="popisek">
                     <div className="adresa">
